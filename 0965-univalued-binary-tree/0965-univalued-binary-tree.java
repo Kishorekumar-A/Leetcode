4ab489a11f6cdd73f8075f1a5e3fa43afaf6992d
Kishorekumar-A <23cs032@nandhaengg.org>
2026-06-22 09:49:14 +0530

    Time: 0 ms (100%), Space: 43 MB (37.38%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.java
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -16,27 +16,35 @@
 class Solution {
 
      
-    public static void inorder(TreeNode root,List<Integer> li)
-    {
+    // public static void inorder(TreeNode root,List<Integer> li)
+    // {
         
-        if(root==null) return;
-        inorder(root.left,li);
-        li.add(root.val);
-        inorder(root.right,li);
+    //     if(root==null) return;
+    //     inorder(root.left,li);
+    //     li.add(root.val);
+    //     inorder(root.right,li);
 
+    // }
+    // public boolean isUnivalTree(TreeNode root) {
+    //     List<Integer> li=new ArrayList<>();
+    //     inorder(root,li);
+    //     int t=li.get(0);
+    //     System.out.println(li);
+    //     for(int i: li)
+    //     {
+    //         if(t!=i) return false;
+    //     }
+    //     return true;
+    // }
+    public static boolean uni(TreeNode root, int v)
+    {
+        if(root==null) return true;
+        if(root. val!=v) return false;
+
+        return uni(root.left,v)&&uni(root.right,v);
     }
     public boolean isUnivalTree(TreeNode root) {
-        List<Integer> li=new ArrayList<>();
-        inorder(root,li);
-        int t=li.get(0);
-        System.out.println(li);
-        for(int i: li)
-        {
-            if(t!=i) return false;
-        }
-        return true;
+        if(root==null) return true;
+        return uni(root,root.val);        
     }
-    // public boolean isUnivalTree(TreeNode root) {
-    //     if()
-    // }
 }
