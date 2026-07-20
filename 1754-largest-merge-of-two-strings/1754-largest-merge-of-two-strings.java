02017e25b508775150b1b687f250fa316cb5f4e3
Kishorekumar-A <23cs032@nandhaengg.org>
2026-07-20 12:17:03 +0530

    Time: 88 ms (7%), Space: 47.8 MB (9.73%) - LeetHub

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.java
@@ -0,0 +1,61 @@
+class Solution {
+    public String largestMerge(String word1, String word2) {
+        int i=0,j=0,n=word1.length(),m=word2.length();
+        String re="";
+        // while(i<n&&j<m)
+        // {
+
+        //     if(word1.charAt(i)<word2.charAt(j))
+        //     {
+        //         re+=word2.charAt(j);
+        //         j++;
+        //     }
+        //     else if(word1.charAt(i)==word2.charAt(j))
+        //     {
+        //         if(n-i>m-j)
+        //         {
+        //             re+=word1.charAt(i);
+        //             i++;
+        //         }
+        //         else
+        //         {
+        //             re+=word2.charAt(j);
+        //             j++;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         re+=word1.charAt(i);
+        //         i++;
+        //     }
+        //     System.out.println(re);
+        // }
+
+        while(i<n&&j<m)
+        {
+            String w1=word1.substring(i,n);
+            String w2=word2.substring(j,m);
+            if(w1.compareTo(w2)<0)
+            {
+                re+=word2.charAt(j);
+                j++;
+            }
+            else
+            {
+                re+=word1.charAt(i);
+                i++;
+            }
+        }
+        while(i<word1.length())
+        {
+            re+=word1.charAt(i);
+            i++;
+        }
+        while(j<word2.length())
+        {
+            re+=word2.charAt(j);
+            j++;
+        }
+        return re;
+    }
+}
