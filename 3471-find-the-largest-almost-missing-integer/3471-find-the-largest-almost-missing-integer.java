8042737076fde40c47bbb97f2f173ff46a635106
Kishorekumar-A <kishorekumarcse100@gmail.com>
2026-08-19 14:15:30 +0530

    Time: 6 ms (38.25%), Space: 44.8 MB (69.12%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,22 +1,5 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        // int f=nums[0],l=nums[nums.length-1];
-        // boolean lf=true,ff=true;
-        // for(int i=1;i<nums.length-1;i++)
-        // {
-        //     if(nums[i]==f) ff=false;
-        //     if(nums[i]==l) lf=false;
-        // }
-        // if(lf&&ff)
-        // {
-        //     if(k==1&&l==f) return -1;
-            
-        //     // return -1;
-        // }
-        // if(!ff&&lf) return l;
-        // if(ff&&!lf) return f;
-        // return(l>=f)? l: f;
-
         List<Integer> li=new ArrayList<>();
         int m=0;
         for(int i:nums)
@@ -32,14 +15,11 @@ class Solution {
             {
                 if(li.contains(nums[j]))
                 {
-                    // System.out.print(nums[j]+" ");
                     fr[li.indexOf(nums[j])]++;
                 }
             }
         }
-                // System.out.println(li);
         int max=-1;
-        System.out.println(Arrays.toString(fr));
         for(int i=0;i<fr.length;i++)
         {
             if(fr[i]==1)
