267063c0b3552c341c45ed2328277167ccea82a8
Kishorekumar-A <kishorekumarcse100@gmail.com>
2026-09-25 09:52:13 +0530

    Time: 0 ms (100%), Space: 13.4 MB (18.2%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        m={}
+        for i in range(len(nums)):
+            c=target-nums[i]
+            if(c in m):
+                return [m[c],i]
+            m[nums[i]] =i        
